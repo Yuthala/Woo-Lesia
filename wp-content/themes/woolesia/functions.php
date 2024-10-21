8e31bcf3392964bf668f792c0cb6be29503de345
Yuthala <dinapolousova@gmail.com>
2024-10-21 12:59:10 +0300

    disable woo styles

--- wp-content/themes/woolesia/functions.php
+++ wp-content/themes/woolesia/functions.php
@@ -17,3 +17,5 @@ add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_script( 'woolesia-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), false, true );
 	wp_enqueue_script( 'woolesia-mainjs',  get_template_directory_uri() . '/assets/js/main.js' );
 } );
+
+require_once get_template_directory() . '/incs/woocommerce-hooks.php';
